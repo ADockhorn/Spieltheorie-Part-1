090ba67d9f8d3ac930c0db90d013128806944b94
Alexander Dockhorn <dockhorn.alexander@gmail.com>
2023-04-24 17:01:29 +0200

    finished girls day files

---
+++ js/lib/sharing.js
@@ -0,0 +1,21 @@
+window.addEventListener("load",function(){
+	/*
+	// Find the "sharing" dom
+	var sharingDOM = document.body.querySelector("sharing");
+
+	// URL encodeable
+	var title = sharingDOM.getAttribute("title");
+	var text = sharingDOM.getAttribute("text");
+	var link = sharingDOM.getAttribute("link");
+	text = encodeURIComponent(text);
+	link = encodeURIComponent(link);
+
+
+	// Create full html
+	var sharing = document.createElement("div");
+	sharing.className = "sharing";
+
+	// Replace it in the dom
+	sharingDOM.parentNode.replaceChild(sharing, sharingDOM);
+	*/
+});
